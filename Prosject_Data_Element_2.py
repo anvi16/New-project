e1a98816fc8cc2d4bcd675666572151aeb582fd9
Andreas Vik <swews92@gmail.com>
2020-11-13 16:40:59 +0100

    Oppdaterte filer

---
+++ Prosject_Data_Element_2.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df = pd.read_csv("datasets\\Weather\\2342202.csv")
+
+print(df.info())
+print(df.head())
+# datasette er organisert etter 
+#1. verstasjon
+#2. Navn på område som bil måly
+#3. Dato of tid for måling
+#4. TAVG som er en målt verdi (farenheit?)
+# Datatyper er floats og Objects
+
+
+
+
+ 
